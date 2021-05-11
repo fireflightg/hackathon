3829e47a7d6562702bac0c7cd7033c4514d2ced7
Saahib Mohammed <35312792+fireflightg@users.noreply.github.com>
2021-05-11 08:35:01 -0400

    s

--- js/indoormap.js
+++ js/indoormap.js
@@ -105,63 +105,7 @@ function lis() {
 
 
 
-// var id, target, option;
-// var long, lat
-// navigator.geolocation.getCurrentPosition(function(position) {
-//     var print = document.getElementById('print');
-//     console.log(position.coords.longitude);
-//     lat = position.coords.latitude;
-//     long = position.coords.longitude;
-//     console.log(position.coords.latitude);
-//     print.innerText = "longitude: " + position.coords.longitude + "latitude: " + position.coords.latitude;
 
-
-// });
-// var grabber = firebase.database().ref().child(user.uid);
-// var databa = grab.child("NearestStore");
-// databa.on('value', snap => {
-//     console.log(snap.val().lat);
-//     snap.val().lat;
-// });
-// var c = document.getElementById("myCanvas");
-// var ctx = c.getContext("2d");
-// ctx.beginPath();
-// ctx.rect(20, 20, position.coords.longitude, position.coords.latitude);
-// ctx.stroke();
-
-// function success(pos) {
-//     var crd = pos.coords;
-
-//     if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
-//         var print = document.getElementById('print');
-//         console.log('Congratulation, you reach the target');
-//         print.innerText = "longitude: " + position.coords.longitude + "latitude: " + position.coords.latitude;
-//         navigator.geolocation.clearWatch(id);
-//     }
-//     var c = document.getElementById("myCanvas");
-//     var ctx = c.getContext("2d");
-//     ctx.beginPath();
-//     ctx.rect(20, 20, 150, 100);
-//     ctx.stroke();
-// };
-
-// function error(err) {
-//     console.warn('ERROR(' + err.code + '): ' + err.message);
-// };
-
-// target = {
-//     latitude: lat,
-//     longitude: long,
-// }
-
-// options = {
-//     enableHighAccuracy: true,
-//     timeout: 5000,
-//     maximumAge: 0
-// };
-
-// id = navigator.geolocation.watchPosition(success, error, options);
-// window.onload = Geo.locate();
 var Geo = {
 
     locate: function() {
